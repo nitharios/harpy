517c7b5e975151b72f53f455f5c30c62b65411b4
Nathan N <Nitharios@users.noreply.github.com>
2018-09-03 11:43:00 -1000

    Fixed typo in titles

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TerraVion</h1>
+          <h1 className="App-title">Welcome to TerrAvion</h1>
         </header>
       </div>
     );
