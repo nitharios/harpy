ff67966c6163a45bb20b1e726e263c988bf87e38
Nathan N <Nitharios@users.noreply.github.com>
2018-09-03 14:33:09 -1000

    Added tokens and necessary leaflet variables

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import L from 'leaflet';
+
 import './App.css';
 
+const apiUrl = 'https://api2.terravion.com';
+const access_token = '2e68cee0-b2fd-4ef5-97f6-8e44afb09ffa';
+const user_id = '5bad4dfa-7262-4a0a-b1e5-da30793cec65';
+const epochStart = '1456200627'
+const epochEnd = '1456632627'
+const center = [38.540580, -121.877271];
+const zoom = 15;
+
 class App extends Component {
+  componentDidMount() {
+    this.map = this.generateMap();
+  }
+
+  generateMap() {
+    return L.map('map', {
+      center: center,
+      zoom: zoom,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TerrAvion</h1>
-        </header>
-      </div>
+      <div id="map"></div>
     );
   }
 }
